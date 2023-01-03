fe5281608598baf6bc8c5ec2b1e27283ca046078
laczko090 <laczko090@gmail.com>
2023-01-03 10:01:57 +0100

    Add simple webcomponent

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+const template = document.createElement('template');
+template.innerHTML = `<style>h2 { color: red; }</style><h2></h2>`;
+
+class MyProduct extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector('h2').innerText = this.getAttribute('name');
+  }
+}
+
+window.customElements.define('my-product', MyProduct);
